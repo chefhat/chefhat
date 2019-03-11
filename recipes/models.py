ed89f3098a2b35bb659b60d708d22f0cd9b447ac
Peter Lavigne <peter.lavigne@appfolio.com>
2019-03-10 22:30:50 -0700

    create recipe model

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Recipe(models.Model):
+    title = models.CharField(max_length=80)
+    description = models.TextField()
+    forked_from = models.ForeignKey(
+      'self',
+      default=None,
+      on_delete=models.SET_DEFAULT,
+      blank=True,
+      null=True)
