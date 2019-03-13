e15347f71e60d6f1cdabddea6aea65acb65a290b
NoahMezher <noahmezher@gmail.com>
2019-03-12 22:26:07 -0400

    Added Google login.

--- chefhat/urls.py
+++ chefhat/urls.py
@@ -14,13 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf.urls import url, include
 from django.contrib import admin
-from django.urls import include, path
-from . import views
+from django.contrib.auth import views
+from .views import home
 
 urlpatterns = [
-    path('', views.home),
-    path('admin/', admin.site.urls),
-
-    path('recipes/', include('recipes.urls')),
+    url(r'^$', home, name='home'),
+    url(r'^admin/', admin.site.urls),
+    url(r'^login/$', views.LoginView.as_view(), name='login'),
+    url(r'^logout/$', views.LogoutView.as_view(), name='logout'),
+    url(r'^auth/', include('social_django.urls', namespace='social')),
+    url(r'^recipes/', include('recipes.urls')),
 ]
